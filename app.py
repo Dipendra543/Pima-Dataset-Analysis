b2d2205cdc94e7e2ee1e4ef0e33d16becb3cff23
Dipendra543 <karkidipendra543@gmail.com>
2019-07-20 17:52:44 -0500

    some minor changes in the messages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Define a python framework for a REST API for diabetic prediction
 
-from MachineLearning import MachineLearning
+from machinelearning import MachineLearning
 from send_sms import send_sms
 
 import pickle
@@ -17,19 +17,21 @@ def train_model():
     if request.method == 'POST':
         success_status = True
         global trained_models
-        ml_model = MachineLearning('diabetes.csv')
+        ml_model = MachineLearning('./data/diabetes.csv')
         ml_model.svm()
         ml_model.logistic_regression()
         ml_model.decision_tree()
-        ml_model.persist_model()
+        ml_model.persist_model()  # This line will save the pickle files to the ./output/ directory
         trained_models = ml_model.filenames
 
         return render_template("trainmodels.html", success_status=success_status, trained_models=trained_models)
 
 
-def get_model():
+def get_model(type_to_learn):
     global loaded_models
-    with open('./output/svm_linear.pkl', 'rb') as f:
+    filename = './output/'+type_to_learn+'.pkl'
+    #print(filename)
+    with open(filename, 'rb') as f:
         loaded_models = pickle.load(f)
 
 
@@ -41,18 +43,20 @@ def home():
 @app.route('/predict', methods=['POST'])
 def get_prediction():
     if request.method == 'POST':
-        get_model()
         output_values = ["Non-Diabetic", "Diabetic"]
         data = request.form
         data = dict(data)
+        type_to_learn = data.pop("types_to_learn", None)
+        get_model(type_to_learn)
+        # print(data, types_to_learn)
         data = pd.DataFrame(data, index=[0])
         prediction = loaded_models.predict(data)
         prediction_string = str(output_values[prediction[0]])
         return_str = f'The Prediction is: {prediction_string}'
-        send_sms(prediction_string)
+        send_sms(prediction_string, type_to_learn)
     return return_str
 
 
 if __name__ == "__main__":
-    get_model()
+    # get_model()
     app.run(host="0.0.0.0")
--- send_sms.py
+++ send_sms.py
@@ -2,16 +2,17 @@ import os
 from twilio.rest import Client
 
 
-def send_sms(message):
+def send_sms(message, type_to_learn):
     account_sid = os.environ["TWILIO_ACCOUNT_SID"]
     auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 
     client = Client(account_sid, auth_token)
 
     client.messages.create(
-        to="+16825514241",
+        to="+19728549147",
         from_="+19728959063",
-        body=f'Hey Buddy, The prediction for your diabetic diagnosis is: {message}'
+        body=f'Hey Buddy, The prediction for your diabetic diagnosis is: {message}. The prediction was implemented using'
+        f' the method {type_to_learn}'
 
     )
 
