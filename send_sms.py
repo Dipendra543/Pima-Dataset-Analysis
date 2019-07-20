94dff9947085214de2fb4ed2bc26d9a651436eb9
Dipendra543 <karkidipendra543@gmail.com>
2019-07-20 18:16:40 -0500

    minor changes

--- send_sms.py
+++ send_sms.py
@@ -12,7 +12,7 @@ def send_sms(message, type_to_learn):
         to="+19728549147",
         from_="+19728959063",
         body=f'Hey Buddy, The prediction for your diabetic diagnosis is: {message}. The prediction was implemented using'
-        f' the method {type_to_learn}'
+        f' the method: "{type_to_learn}"'
 
     )
 
